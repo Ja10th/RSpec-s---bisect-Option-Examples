2a10d6ffddaeb16e4f287f99d4ef380995368a29
James Oluwaleye <100203073+Ja10th@users.noreply.github.com>
2024-04-17 13:44:57 +0100

    Create calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,45 @@
+# calculator.rb
+
+def accept_operands
+  print "Enter first number: "
+  operand1 = gets.to_i
+  print "Enter second number: "
+  operand2 = gets.to_i
+  [operand1, operand2]
+end
+
+loop do
+  puts "--- Calculator ---"
+  puts "1. Addition"
+  puts "2. Subtraction"
+  puts "3. Multiplication"
+  puts "4. Division"
+  puts "5. Exit"
+  print "Which operation do you want to perform: "
+  operation = gets.to_i
+
+  break if operation == 5
+
+  case operation
+  when 1
+    num1, num2 = accept_operands()
+    res = num1 + num2
+    puts "#{num1} + #{num2} = #{res}"
+  when 2
+    num1, num2 = accept_operands()
+    res = num1 - num2
+    puts "#{num1} - #{num2} = #{res}"
+  when 3
+    num1, num2 = accept_operands()
+    res = num1 * num2
+    puts "#{num1} * #{num2} = #{res}"
+  when 4
+    num1, num2 = accept_operands()
+    res = num1.to_f / num2
+    puts "#{num1} / #{num2} = #{res}"
+  else
+    puts "Invalid choice"
+  end
+
+  puts "" # Print a newline for the next iteration
+end
